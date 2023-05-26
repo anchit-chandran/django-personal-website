356574efb632c31129772b6ca3d84dd0cac156ab
Anchit <anchit97123@gmail.com>
2023-05-26 20:52:27 +0100

    adds navigation and view individual posts
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- app_website/urls.py
+++ app_website/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("projects", views.projects, name="projects"),
+    path("blog", views.blog, name="blog"),
+    path("blog/post/<int:post_id>", views.view_post, name='view_post'),
+    path("contact", views.contact, name="contact"),
 ]
--- app_website/views.py
+++ app_website/views.py
@@ -5,11 +5,31 @@ from .models import (
     BlogPost,
 )
 
+
 # Create your views here.
 def index(request):
-    
-    blog_posts = BlogPost.objects.filter(featured=True)
-    
-    return render(request, 'app_website/index.html', {
-        'posts' : blog_posts 
-    })
+    featured_blog_posts = BlogPost.objects.filter(featured=True)
+
+    return render(request, "app_website/index.html", {"posts": featured_blog_posts})
+
+
+def view_post(request, post_id):
+    post = BlogPost.objects.get(id=post_id)
+    return render(request, "app_website/post.html", {'post':post})
+
+
+def about(request):
+    return render(request, "app_website/about.html")
+
+
+def projects(request):
+    return render(request, "app_website/projects.html")
+
+
+def blog(request):
+    posts = BlogPost.objects.all()
+    return render(request, "app_website/blog.html", {'posts':posts})
+
+
+def contact(request):
+    return render(request, "app_website/contact.html")
