b6c1bd2962132131a28ee63140b41f6ebf785638
anchit-chandran <anchit97123@gmail.com>
2023-06-21 08:04:49 +0100

    misc refactor
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- app_website/views.py
+++ app_website/views.py
@@ -19,7 +19,14 @@ from .models import (
 from .constants import (
     DEFAULT_HEADER_IMG_URL,
 )
-from .forms import LoginForm, CreateDraftPostForm, PublishPostForm, SubscribeForm, EditSubscriberForm, EditPostForm
+from .forms import (
+    LoginForm,
+    CreateDraftPostForm,
+    PublishPostForm,
+    SubscribeForm,
+    EditSubscriberForm,
+    EditPostForm,
+)
 from app_website.general_functions import (
     get_first_p_from_md,
 )
@@ -27,56 +34,59 @@ from app_website.general_functions import (
 
 # Create your views here.
 def index(request):
-    
     # person hits subscribe to newsletter
     if request.method == "POST":
-        
         form = SubscribeForm(request.POST)
-        
-        if form.is_valid():
-            
-            # ensure email not already used
-            if Subscriber.objects.filter(email=form.cleaned_data['email']).exists():
-                
-                # add email already used message
-                messages.error(request, "Uh oh, looks like that email's already signed up 😲", extra_tags='danger')
 
-                return redirect('index')
-            
+        if form.is_valid():
+            # ensure email not already used
+            if Subscriber.objects.filter(email=form.cleaned_data["email"]).exists():
+                # add email already used message
+                messages.error(
+                    request,
+                    "Uh oh, looks like that email's already signed up 😲",
+                    extra_tags="danger",
+                )
+
+                return redirect("index")
+
             form.cleaned_data["subscribed"] = True
             subscriber = form.save()
-            
+
             subscriber = Subscriber.objects.get(id=subscriber.id)
-            
+
             if not subscriber.confirmation_email_sent:
-            
                 # get details for confirmation email
                 subject = f"Welcome, {subscriber.name}"
-                html_message = render_to_string('app_website/mailing_list_confirm_subscription.html',{'subscriber':subscriber})
+                html_message = render_to_string(
+                    "app_website/mailing_list_confirm_subscription.html",
+                    {"subscriber": subscriber},
+                )
                 plain_text = strip_tags(html_message)
-                
+
                 # send confirmation email
                 send_mail(
-                        subject=subject,
-                        message=plain_text,
-                        html_message=html_message,
-                        from_email="anchit97123@gmail.com",
-                        recipient_list=[subscriber.email],
-                        fail_silently=False,
-                    )
-                
+                    subject=subject,
+                    message=plain_text,
+                    html_message=html_message,
+                    from_email="anchit97123@gmail.com",
+                    recipient_list=[subscriber.email],
+                    fail_silently=False,
+                )
+
                 # update .confirmation_email_sent field
                 subscriber.confirmation_email_sent = True
                 subscriber.save()
-                
+
                 # add success message
-                messages.success(request, "Thanks! You should receive an email confirmation soon 📩")
-                
-                return redirect('index')
-            
+                messages.success(
+                    request, "Thanks! You should receive an email confirmation soon 📩"
+                )
+
+                return redirect("index")
 
     # get featured blog posts
-    featured_blog_posts = BlogPost.objects.filter(published=True,featured=True)
+    featured_blog_posts = BlogPost.objects.filter(published=True, featured=True)
 
     # get projects
     if Project.objects.filter(featured=True).count() >= 3:
@@ -85,7 +95,6 @@ def index(request):
         featured_projects = Project.objects.all()[:3]
 
     for project in featured_projects:
-        
         project.content = get_first_p_from_md(project)
 
     # get subscribe modal form
@@ -102,27 +111,17 @@ def index(request):
         },
     )
 
+
 def blog(request):
-    
     posts = BlogPost.objects.filter(published=True).order_by("-posted_at")
 
     return render(request, "app_website/blog.html", {"posts": posts})
+
 
 def view_post(request, post_id):
     post = BlogPost.objects.get(id=post_id)
 
     return render(request, "app_website/post.html", {"post": post})
-
-
-def view_project(request, project_id):
-    
-    project = Project.objects.get(id=project_id)
-
-    return render(request, "app_website/project.html", {"project": project})
-
-
-def about(request):
-    return render(request, "app_website/about.html")
 
 
 def projects(request):
@@ -132,6 +131,16 @@ def projects(request):
         project.content = get_first_p_from_md(project)
 
     return render(request, "app_website/projects.html", {"projects": projects})
+
+
+def view_project(request, project_id):
+    project = Project.objects.get(id=project_id)
+
+    return render(request, "app_website/project.html", {"project": project})
+
+
+def about(request):
+    return render(request, "app_website/about.html")
 
 
 def contact(request):
@@ -145,7 +154,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            # Redirect to a success page.
+
             return redirect("index")
 
     form = LoginForm()
@@ -167,36 +176,35 @@ def admin_user_view(request):
 
 @login_required
 def create_draft_post(request):
-    
     if request.method == "POST":
-        
         form = CreateDraftPostForm(request.POST, initial={"published": False})
 
         if form.is_valid():
-            
             # post not featured, don't need to check N featured posts
-            if not form.cleaned_data['featured']:
-
+            if not form.cleaned_data["featured"]:
                 form.save()
-                
+
                 return redirect("view_posts_admin")
-            
+
             # Request post to be featured - check not too many featured posts
-            current_featured_posts_count = BlogPost.objects.filter(featured=True).count()
-            
+            current_featured_posts_count = BlogPost.objects.filter(
+                featured=True
+            ).count()
+
             # already have too many posts
             if current_featured_posts_count >= 3:
-                
-                messages.error(request,'Already have 3 featured posts! Un-feature at least one of them to make this a featured post.', extra_tags="danger")
-                
+                messages.error(
+                    request,
+                    "Already have 3 featured posts! Un-feature at least one of them to make this a featured post.",
+                    extra_tags="danger",
+                )
+
                 return render(request, "app_website/create_post.html", {"form": form})
 
         else:
-            
-            messages.error(request, form.errors, extra_tags='danger')
-            
-            return render(request, 'app_website/create_post.html',{'form':form})
-            
+            messages.error(request, form.errors, extra_tags="danger")
+
+            return render(request, "app_website/create_post.html", {"form": form})
 
     form = CreateDraftPostForm()
 
@@ -205,8 +213,7 @@ def create_draft_post(request):
 
 @login_required
 def view_posts_admin(request):
-    
-    posts = BlogPost.objects.all().order_by("published",'-posted_at')
+    posts = BlogPost.objects.all().order_by("published", "-posted_at")
 
     return render(request, "app_website/view_posts_admin.html", {"posts": posts})
 
@@ -216,42 +223,41 @@ def edit_post(request, post_id):
     post = BlogPost.objects.get(id=post_id)
 
     if request.method == "POST":
-        
         form = EditPostForm(request.POST, instance=post)
-        
+
         if form.is_valid():
-            
-            # post not featured, don't need to check N featured posts
-            if not form.cleaned_data['featured']:
-
-
+            # Check if post not featured as don't need to check N featured posts
+            if not form.cleaned_data["featured"]:
                 form.save()
-                
+
                 return redirect("view_post", post_id=post_id)
-            
+
             # Request post to be featured - check not too many featured posts, excluding this post
-            current_featured_posts_count = BlogPost.objects.filter(featured=True).exclude(title=form.cleaned_data['title']).count()
-            
+            current_featured_posts_count = (
+                BlogPost.objects.filter(featured=True)
+                .exclude(title=form.cleaned_data["title"])
+                .count()
+            )
+
             # already have too many posts
             if current_featured_posts_count >= 3:
-        
-                
-                messages.error(request,'Already have 3 featured posts! Un-feature at least one of them to make this a featured post.', extra_tags="danger")
-                
+                messages.error(
+                    request,
+                    "Already have 3 featured posts! Un-feature at least one of them to make this a featured post.",
+                    extra_tags="danger",
+                )
+
                 return render(request, "app_website/edit_post.html", {"form": form})
-            
+
             else:
-
                 form.save()
-                
+
                 return redirect("view_post", post_id=post_id)
-        
+
         else:
+            messages.error(request, form.errors, extra_tags="danger")
 
-            messages.error(request, form.errors, extra_tags='danger')
-            
-            return render(request, 'app_website/edit_post.html',{'form':form})
-
+            return render(request, "app_website/edit_post.html", {"form": form})
 
     form = EditPostForm(instance=post)
 
@@ -305,8 +311,11 @@ def publish_draft_post(request, post_id):
                     recipient_list=subscribers,
                     fail_silently=False,
                 )
-                
-                messages.success(request,f'Post published and emailed out to {len(subscribers)} subscribers 🔥')
+
+                messages.success(
+                    request,
+                    f"Post published and emailed out to {len(subscribers)} subscribers 🔥",
+                )
 
                 return redirect("blog")
 
@@ -317,12 +326,15 @@ def publish_draft_post(request, post_id):
         {"post": post, "form": form, "subscriber_count": subscriber_count},
     )
 
+
 @login_required
 def view_projects_admin(request):
-    
-    projects = Project.objects.all().order_by('-posted_at')
+    projects = Project.objects.all().order_by("-posted_at")
 
-    return render(request, "app_website/view_projects_admin.html", {"projects": projects})
+    return render(
+        request, "app_website/view_projects_admin.html", {"projects": projects}
+    )
+
 
 @login_required
 def view_subscribers(request):
@@ -351,4 +363,3 @@ def edit_subscriber(request, subscriber_id):
         "app_website/view_subscriber.html",
         {"form": form, "subscriber": subscriber},
     )
-    
